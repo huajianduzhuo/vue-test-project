c2af6c99ea45f273e085ef4a44c9d76bc0ededdd
huajianduzhuo <yujing_m4370@163.com>
2018-06-26 16:33:20 +0800

    菜单 menu

--- src/plugins/Menu/menu.js
+++ src/plugins/Menu/menu.js
@@ -1,3 +1,40 @@
 export default {
-  name: 'Menu'
+  name: 'Menu',
+  props: {
+    event: Event,
+    vNode: Object
+  },
+  methods: {
+    closeMenu () {
+      document.body.style.overflow = ''
+      this.$destroy()
+    }
+  },
+  mounted () {
+    document.body.style.overflow = 'hidden'
+  },
+  render (h) {
+    const docH = document.documentElement.clientHeight
+    const elm = this.vNode.elm
+    const elmW = elm.offsetWidth
+    const elmH = elm.offsetHeight
+    const top = elm.getBoundingClientRect().top
+    const left = elm.getBoundingClientRect().left
+    const menuLeft = left + elmW / 2
+    let direc = ''
+    if (top >= docH / 5) {
+      direc = 'top'
+    } else {
+      direc = 'bottom'
+    }
+    const maskLayer = (<div class="menu-mask"></div>)
+    let menu = direc === 'top'
+      ? (
+        <div style={{left: menuLeft + 'px'}} class="menu menu-top"></div>
+      )
+      : (
+        <div style={{left: menuLeft + 'px'}} class="menu menu-bottom"></div>
+      )
+    return menu
+  }
 }
