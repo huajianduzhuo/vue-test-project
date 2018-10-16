cb819a11b6f4dd56a494751b334d87d20e8f1ebd
huajianduzhuo <yujing_m4370@163.com>
2018-10-16 15:32:30 +0800

    使用vw布局

--- .postcssrc.js
+++ .postcssrc.js
@@ -5,6 +5,27 @@ module.exports = {
     "postcss-import": {},
     "postcss-url": {},
     // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
+    "autoprefixer": {},
+    /* "postcss-px2rem": {
+      remUnit: 100
+    },
+    "postcss-pxtorem": {
+      rootValue: 100,
+      unitPrecision: 5,
+      propList: ['*'],
+      selectorBlackList: [],
+      replace: false,
+      mediaQuery: false,
+      minPixelValue: 1
+    }, */
+    "postcss-px-to-viewport": {
+      viewportWidth: 375,
+      viewportHeight: 667,
+      unitPrecision: 5,
+      viewportUnit: 'vw',
+      selectorBlackList: [],
+      minPixelValue: 1,
+      mediaQuery: false
+    }
   }
 }
